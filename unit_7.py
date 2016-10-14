50504cd63360329349e7b4a9fe8d2937d0ae2747
hambuol <oliver.hamburger@ssfs.org>
2016-10-14 13:08:03 -0400

    final check before turn in

--- unit_7.py
+++ unit_7.py
@@ -1,5 +1,13 @@
+# written by oliver hamburger
+
+# program encodes and decodes a given string
+
+# string of alphabet
 n = "abcdefghijklmnopqrstuvwxyz"
+
+
 def decision():
+    """function takes a decision and runs the apropriet function"""
     while True:
         choice = input("press e to incode, d to decode or q to quit: ")
         if choice == "e":
@@ -7,60 +15,45 @@ def decision():
         elif choice == "d":
             return decode()
         elif choice == "q":
-            return quit()
+            return end()
         else:
             return decision()
 
 
-
-
-
-
-
-
-
-
 def encode():
+    """function takes a string and encodes it with the shifted alphabet made by the given key"""
     string = input("please enter the text to be encoded: ")
     key = int(input("please enter the key (0-25): "))
     r = n[key:26] + n[0:key]
-    print(string.index())
+    encoded = ""
+    for x in string:
+        encoded += r[n.index(x)]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(encoded)
+    decision()
 
 
 def decode():
+    """function takes a string and decodes it with the shifted alphabet made by the given key"""
     string = input("please enter the text to be decoded: ")
     key = int(input("please enter the key (0-25): "))
     r = n[key:26] + n[0:key]
+    decoded = ""
+    for x in string:
+        decoded += n[r.index(x)]
+
+    print(decoded)
+    decision()
 
 
-
-
-
-    
-
-
-
-
-
-
-def quit():
+def end():
+    """function thanks user for playing and ends program"""
     print("Thanks for playing")
 
 
+# function runs decision function to start program
+def main():
+    decision()
 
+main()
 
-decision()
